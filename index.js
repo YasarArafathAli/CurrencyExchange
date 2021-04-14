71576855d8845544e2c78142037d45f99aee3cc9
Yasar Arafath <yasara218@gmail.com>
2021-04-14 22:52:40 +0530

    ui update

---
+++ index.js
@@ -0,0 +1,11 @@
+const currency1 = document.getElementById("currency-one");
+const currency2 = document.getElementById("currency-two");
+const amount1 = document.getElementById("amount-one");
+const amount2 = document.getElementById("amount-two");
+const rate = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+
+function calculate() { }
+
+calculate
